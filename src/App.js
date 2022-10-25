25af4292fa9f0ad576449966e26a0e57f6a622ec
EricMungai97 <mungaikinus@gmail.com>
2022-10-24 19:33:21 -0700

    new components

--- src/App.js
+++ src/App.js
@@ -1,25 +1,33 @@
-import logo from './logo.svg';
-import './App.css';
+import React from 'react';
+// import axios from 'axios';
+import Header from './components/Header.js';
+import City from './components/City.js';
+import Main from './components/Main.js';
+import Footer from './components/Footer.js';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+class App extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      city: '',
+      cityData: [],
+      error: false,
+      errorMessage: ''
+    }
+  }
+
+ 
+  render(){
+    return(
+      <div>
+        <Header />
+        <City />
+        <Main />
+        <Footer />
+      </div>
+    )
+  }
 }
+
 
 export default App;
