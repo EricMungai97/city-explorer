bd9c8d3ac0ec713f52a6c07e7ddc46f4dc0df22b
EricMungai97 <mungaikinus@gmail.com>
2022-10-25 14:57:42 -0700

    completed lab06

--- src/components/Cityform.js
+++ src/components/Cityform.js
@@ -3,12 +3,12 @@ import Form from 'react-bootstrap/Form';
 import Button from 'react-bootstrap/Button';
 import 'bootstrap/dist/css/bootstrap.min.css';
 
-class City extends React.Component {
+class Cityform extends React.Component {
   render(){
     return(
       <Form onSubmit={this.props.handleGetCity}>
       <Form.Group>
-        <Form.Label>Pick a City!</Form.Label>
+        <Form.Label>Pick a City To Explore!</Form.Label>
         <Form.Control type="location" placeholder="Enter city name" onInput={this.props.handleInput} />
       </Form.Group>
       
@@ -19,4 +19,4 @@ class City extends React.Component {
   }
 }
 
-export default City;
+export default Cityform;
--- src/components/Main.js
+++ src/components/Main.js
@@ -1,5 +1,5 @@
 import React from 'react';
-
+import Card from 'react-bootstrap/Card';
 
 class Main extends React.Component {
 
@@ -8,7 +8,17 @@ class Main extends React.Component {
   render(){
     return(
       <div>
-       
+        <Card id='card'>
+        <Card.Body>
+          <Card.Title>{this.props.cityName}</Card.Title>
+          <Card.Text>
+            {this.props.latitude}
+            {this.props.longitude}
+            {this.props.error}
+          </Card.Text>
+        </Card.Body>
+        <Card.Img variant="bottom" src={this.props.place} />
+        </Card>
       </div>
     )
   }
