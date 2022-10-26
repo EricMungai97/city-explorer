4209fbbf690a91a7726d41eca68a10fbda57292a
EricMungai97 <mungaikinus@gmail.com>
2022-10-25 21:44:18 -0700

    complete lab06

---
+++ src/components/Weather.js
@@ -0,0 +1,15 @@
+import React from 'react';
+import Card from 'react-bootstrap/Card';
+
+
+class Weather extends React.Component {
+  render(){
+    return (
+      <div>
+       <Card>{this.props.weatherData}</Card>
+      </div>
+    )
+  }
+}
+
+export default Weather;
