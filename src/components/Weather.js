fd1405fdb6bbf3c04de0d13884367ff906f46a29
EricMungai97 <mungaikinus@gmail.com>
2022-10-26 15:08:58 -0700

    response live

--- src/components/Weather.js
+++ src/components/Weather.js
@@ -1,12 +1,26 @@
 import React from 'react';
 import Card from 'react-bootstrap/Card';
 
-
 class Weather extends React.Component {
-  render(){
+  render() {
+    console.log(this.props.weatherData);
     return (
       <div>
-       <Card>{this.props.weatherData}</Card>
+
+        <Card style={{ width: '18rem' }}>
+
+          
+            {this.props.weatherData.map((day, index) =>
+            <Card.Body>
+            <Card.Title>Forecast Data</Card.Title>
+            <Card.Text>
+              {day.datetime}
+              {day.name}
+            </Card.Text>
+          
+          </Card.Body>
+          )}
+        </Card>
       </div>
     )
   }
