1e8e2bdbcf4de8c1ec9136f30abe1c2bb9d13c9c
EricMungai97 <mungaikinus@gmail.com>
2022-10-27 12:13:38 -0700

    Lab08 Bootstrap complete

--- src/components/Weather.js
+++ src/components/Weather.js
@@ -1,29 +1,41 @@
 import React from 'react';
 import Card from 'react-bootstrap/Card';
+import Container from 'react-bootstrap/Container';
+import Row from 'react-bootstrap/Row';
+import Col from 'react-bootstrap/Col';
 
 class Weather extends React.Component {
   render() {
     console.log(this.props.weatherData);
+    let weatherDat = this.props.weatherData.map((day, index) =>
+    <Col className='m-4'>
+    <Card style={{ width: '18rem' }}>
+      <Card.Body>
+        
+        <Card.Title>Forecast Data</Card.Title>
+        <Card.Text>
+        {day.datetime}
+
+        </Card.Text>
+        <Card.Text>
+        {day.description}
+        </Card.Text>
+        
+      </Card.Body>
+      </Card>
+      </Col>
+    )
     return (
       <div>
+        <Container id='contain' fluid>
+          <Row id='row' xs={1} sm={1} md={2} lg={4} xxl={5} className='g-4'>
+          
+            {weatherDat}
+         
+          </Row>
+          </Container>
 
-        <Card style={{ width: '18rem' }}>
-
-
-          {this.props.weatherData.map((day, index) =>
-            <Card.Body>
-              <Card.Title>Forecast Data</Card.Title>
-              <Card.Text>
-                {day.datetime}
-
-              </Card.Text>
-              <Card.Text>
-                {day.description}
-              </Card.Text>
-
-            </Card.Body>
-          )}
-        </Card>
+       
       </div>
     )
   }
